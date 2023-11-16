1b7966eed62af0a7fc1a15d4da1786e7989fd252
corosto <filipmiera1@gmail.com>
2023-11-16 18:06:01 +0100

    tak zwane gg

--- ChatWithMeFrontend/ChatWithMe/src/app/components/home/components/matches/matches.component.ts
+++ ChatWithMeFrontend/ChatWithMe/src/app/components/home/components/matches/matches.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { Status } from '@components/home/components/matches/api/matches-api.service';
 import { action } from '@components/home/components/matches/components/keyboard-actions/constants/actions.const';
 import { KeyboardActionsComponent } from '@components/home/components/matches/components/keyboard-actions/keyboard-actions.component';
@@ -9,7 +10,7 @@ import { MatchesService } from '@components/home/components/matches/service/matc
 @Component({
   selector: 'matches',
   standalone: true,
-  imports: [CommonModule, KeyboardActionsComponent, MatchComponent],
+  imports: [CommonModule, KeyboardActionsComponent, MatchComponent, MatIconModule],
   providers: [MatchesService],
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.scss'],
@@ -20,7 +21,7 @@ export class MatchesComponent implements OnInit {
   @HostListener('window:resize', ['$event'])
   onResize(event: UIEvent): void {
     const width = (event.target as Window).innerWidth;
-    this.isSmallWindow = width <= 1100;
+    this.isSmallWindow = width <= 900;
   }
 
   isSmallWindow: boolean;
@@ -30,7 +31,7 @@ export class MatchesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.isSmallWindow = window.innerWidth <= 1100;
+    this.isSmallWindow = window.innerWidth <= 900;
   }
 
   actionEvent(action: action): void {
